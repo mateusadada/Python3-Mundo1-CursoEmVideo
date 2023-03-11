7507b6fd658177ba9999ded03cacb10f4e424b26
Mateus Adada <mateusadada@gmail.com>
2023-03-10 23:08:37 -0300

    Update Aula 6 - A.py
    Incluído o resto do código

--- Exercícios/Exercícios resolvidos/Aula 6 - A.py
+++ Exercícios/Exercícios resolvidos/Aula 6 - A.py
@@ -1,1 +1,5 @@
-numero1 = input('Digite um valor: ')
+numero1 = int(input('Digite o primeiro número: '))
+numero2 = int(input('Digite o segundo número: '))
+soma = numero1 + numero2
+
+print(f'\nA soma entre {numero1} e {numero2} é igual a {soma}')
