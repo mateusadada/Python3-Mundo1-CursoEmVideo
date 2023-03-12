a3f4d75787437ce82ad68efdf9143116d44b300e
Mateus Adada <mateusadada@gmail.com>
2023-03-12 13:59:00 -0300

    Create Exercício 004.py

---
+++ Exercícios/Exercícios resolvidos/Exercício 004.py
@@ -0,0 +1,12 @@
+# Leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele.
+
+dado = input('Digite algo: ')
+
+print(f'\nO tipo primitivo desse valor é {type(dado)}')
+print(f'Só tem espaços? {dado.isspace()}')
+print(f'É um número? {dado.isnumeric()}')
+print(f'É alfabético? {dado.isalpha()}')
+print(f'É alfanumérico? {dado.isalnum()}')
+print(f'Está em maiúsculas? {dado.isupper()}')
+print(f'Está em minúsculas? {dado.islower()}')
+print(f'Está capitalizada? {dado.istitle()}')
