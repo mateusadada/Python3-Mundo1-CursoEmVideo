e47efb2cc769dddf3b388ef1750b9cd9afe778bd
Mateus Adada <mateusadada@gmail.com>
2023-03-12 13:15:08 -0300

    Renamed exercises
    Alterado o nome dos exercícios para manter em linha sequencial. Exemplo:
    
    Aula 4 - Desafio 002 virou apenas Desafio 002 e assim sucessivamente

--- Exercícios/Exercícios resolvidos/Aula 4 - Desafio 03.py
+++
@@ -1,9 +0,0 @@
-# Crie um script que leia dois números e mostre a soma entre eles.
-
-print('====== DESAFIO 03 ======')
-
-numero1 = int(input('Digite o primeiro número: '))
-numero2 = int(input('Digite o segundo número: '))
-soma = numero1 + numero2
-
-print(f'A soma entre {numero1} e {numero2} é {soma}')
