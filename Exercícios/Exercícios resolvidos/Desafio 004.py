95a876944aaa470e9c24747a7598f7a96a1fed6f
Mateus Adada <mateusadada@gmail.com>
2023-03-12 13:22:35 -0300

    Update Desafio 004.py
    Adicionado o restante do código.

--- Exercícios/Exercícios resolvidos/Desafio 004.py
+++ Exercícios/Exercícios resolvidos/Desafio 004.py
@@ -4,3 +4,8 @@ resposta = input('Digite qualquer coisa: ')
 
 print(f'\nQual o tipo primitido? {type(resposta)}')
 print(f'É um número? {resposta.isnumeric()}')
+print(f'É alfanúmerico? {resposta.isalnum()}')
+print(f'Está tudo em maiúsculo? {resposta.isupper()}')
+print(f'Tem espaço? {resposta.isspace()}')
+print(f'Contém apenas letras? {resposta.isalpha()}')
+print(f'Tem decimal? {resposta.isdecimal()}')
