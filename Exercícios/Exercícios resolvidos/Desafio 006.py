c1c41956f03536f7cd68cb2d0a66501c2d5c720e
Mateus Adada <mateusadada@gmail.com>
2023-03-12 20:32:14 -0300

    Create Desafio 006.py

---
+++ Exercícios/Exercícios resolvidos/Desafio 006.py
@@ -0,0 +1,7 @@
+# Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
+
+numero = int(input('Digite um número: '))
+
+print(f'\nDobro: {numero * 2}')
+print(f'Triplo: {numero * 3}')
+print(f'Raiz quadrada: {numero ** (1/2):.2f}')
