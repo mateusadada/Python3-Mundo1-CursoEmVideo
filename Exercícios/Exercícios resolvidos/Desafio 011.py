006358cce0190c923efc252a0043d52288e8c869
Mateus Adada <mateusadada@gmail.com>
2023-03-12 23:14:05 -0300

    Create Desafio 011-012-013.py

---
+++ Exercícios/Exercícios resolvidos/Desafio 011.py
@@ -0,0 +1,7 @@
+# Leia a largura e a altura de uma parede em metros, calcule a área e a quantidade de tinta necessária para pintá-la.
+# Sabendo que cada litro de tinta pinta uma área de 2 m².
+
+largura = float(input('Digite a largura de uma parede: '))
+altura = float(input('Digite a altura: '))
+area = largura * altura
+tinta_necessaria =
---
+++ Exercícios/Exercícios resolvidos/Desafio 013.py
@@ -0,0 +1,1 @@
+# Leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
