6cb38a3757e7fd3acec759ae7e835cb5d96b9f87
Mateus Adada <mateusadada@gmail.com>
2023-03-13 22:48:09 -0300

    Create Exercício 005.py

---
+++ Exercícios/Exercícios resolvidos/Exercício 005.py
@@ -0,0 +1,7 @@
+# Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor.
+
+numero = int(input('Digite um número: '))
+antecessor = numero - 1
+sucessor = numero + 1
+
+print(f'\nAnalisando o valor {numero}, seu antecessor é {antecessor} e o sucessor é {sucessor}')
