16d778c44188154bf8ba4443af2e32afa77e34cb
Mateus Adada <mateusadada@gmail.com>
2023-03-14 17:59:38 -0300

    Create Exercício 006.py

---
+++ Exercícios/Exercícios resolvidos/Exercício 006.py
@@ -0,0 +1,11 @@
+# Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
+
+numero = int(input('Digite um número: '))
+dobro = numero * 2
+triplo = numero * 3
+raiz_quadrada = numero ** (1/2)
+
+
+print(f'\nO dobro de {numero} vale {dobro}'
+      f'\nO triplo de {numero} vale {triplo}'
+      f'\nA raiz quadrada de {numero} é igual a {raiz_quadrada:.2f}')
