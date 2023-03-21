21af45660df2a26c3cc278e5fcb8eea5e3c760ee
Mateus Adada <mateusadada@gmail.com>
2023-03-20 23:25:36 -0300

    Update Exercício 007.py
    Alterado o input para a pergunta ficar igual ao vídeo do curso.

--- Exercícios/Exercícios resolvidos/Exercício 007.py
+++ Exercícios/Exercícios resolvidos/Exercício 007.py
@@ -1,7 +1,7 @@
 # Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
 
-nota1 = float(input('Digite a primeira nota: '))
-nota2 = float(input('Digite a segunda nota: '))
+nota1 = float(input('Primeira nota do aluno: '))
+nota2 = float(input('Segunda nota do aluno: '))
 media = (nota1 + nota2)/2
 
-print(f'\nA média entre {nota1:.1f} e {nota2:.1f} é {media:.1f}')
+print(f'\nA média entre {nota1:.1f} e {nota2:.1f} é igual a {media:.1f}')
