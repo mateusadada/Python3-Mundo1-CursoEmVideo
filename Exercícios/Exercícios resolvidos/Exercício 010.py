d83298c8795ebb6f8be450a752b1db1a48ca3aa5
Mateus Adada <mateusadada@gmail.com>
2023-03-23 21:40:48 -0300

    Update Exercício 010.py
    - Alterado a cotação do dólar para o atual.
    - Incluído outras moedas.

--- Exercícios/Exercícios resolvidos/Exercício 010.py
+++ Exercícios/Exercícios resolvidos/Exercício 010.py
@@ -2,6 +2,11 @@
 
 dinheiro = float(input('Quanto dinheiro você tem na carteira? R$ '))
 
-dolares = (dinheiro / 3.27)
+dolares = dinheiro / 5.30
+euro = dinheiro / 5.74
+iene = dinheiro / 24.67
 
-print(f'\nCom R$ {dinheiro:.2f} reais você pode comprar US$ {dolares:.2f} dólares')
+print(f'\nCom R$ {dinheiro:.2f} reais você pode comprar:'
+      f'\nUS$ {dolares:.2f} dólares'
+      f'\nEUR {euro:.2f} euros'
+      f'\nJPY {iene:.2f} ienes')
