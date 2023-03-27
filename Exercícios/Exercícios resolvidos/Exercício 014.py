7ebdc68f6c1e9790acd8fd63ee2a8cc0b834651e
Mateus Adada <mateusadada@gmail.com>
2023-03-26 23:11:26 -0300

    Create Exercício 014.py

---
+++ Exercícios/Exercícios resolvidos/Exercício 014.py
@@ -0,0 +1,7 @@
+# Escreva um programa que converta uma temperatura digitado em graus Celsius e converta para graus Fahrenheit.
+
+graus_celsius = float(input('Informe a temperatura em ºC: '))
+
+graus_fahrenheit = (1.8 * graus_celsius) + 32
+
+print(f'\nA temperatura de {graus_celsius}ºC corresponde a {graus_fahrenheit:.1f}ºF!')
