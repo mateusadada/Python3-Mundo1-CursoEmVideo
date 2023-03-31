b8029c0d9d305a302d301bb17d30cf255485d7b0
Mateus Adada <mateusadada@gmail.com>
2023-03-31 19:45:41 -0300

    Update Desafio 017, 018, 019, 020 e 021
    Atualizado as observações

--- Exercícios/Exercícios resolvidos/Desafio 020.py
+++ Exercícios/Exercícios resolvidos/Desafio 020.py
@@ -1,2 +1,3 @@
 # O mesmo professor do desafio 019 quer sortear a ordem de apresentação de trabalhos dos alunos.
 # Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
+# import random = ver se precisa incluir o código para poder sortear
