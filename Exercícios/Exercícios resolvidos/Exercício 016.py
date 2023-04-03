aafe63ee64d31935781b3d69c33cb821e3aedc0f
Mateus Adada <mateusadada@gmail.com>
2023-04-02 22:29:36 -0300

    Update Exercício 016.py
    Incluído o código

--- Exercícios/Exercícios resolvidos/Exercício 016.py
+++ Exercícios/Exercícios resolvidos/Exercício 016.py
@@ -1,3 +1,9 @@
 # Crie um programa que leia um número real qualquer pelo teclado e mostre na tela a sua porção inteira.
 # Ex: Digite um número: 6.127
 # O número 6.127 tem a parte inteira 6.
+
+import math
+
+numero = float(input('Digite um valor: '))
+
+print(f'\nO número {numero} tem a parte inteira {math.floor(numero)}')
