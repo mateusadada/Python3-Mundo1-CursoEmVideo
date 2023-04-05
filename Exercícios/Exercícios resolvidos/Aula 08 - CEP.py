b21398214ede03d7949e41e4caeb4bb98bcab491
Mateus Adada <mateusadada@gmail.com>
2023-04-04 23:27:16 -0300

    Update Aula 08 - CEP.py
    Adicionado para mostrar vários informações a partir do CEP informado

--- Exercícios/Exercícios resolvidos/Aula 08 - CEP.py
+++ Exercícios/Exercícios resolvidos/Aula 08 - CEP.py
@@ -1,3 +1,10 @@
 from pycep import PyCep
 
 cep = PyCep(input('Digite seu CEP (apenas números): '))
+
+print('\nCEP:', cep.dadosCep['cep'],
+      '\nLogradouro:', cep.dadosCep['logradouro'],
+      '\nComplemento:', cep.dadosCep['complemento'],
+      '\nBairro:', cep.dadosCep['bairro'],
+      '\nCidade:', cep.dadosCep['localidade'], '/', cep.dadosCep['uf'],
+      '\nDDD:', cep.dadosCep['ddd'])
