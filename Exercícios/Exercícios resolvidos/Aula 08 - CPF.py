f64125f01a8c353457118cbff0cbb1365dcb8862
Mateus Adada <mateusadada@gmail.com>
2023-04-24 18:28:24 -0300

    Update Exercício 017.py
    Added the code and a note about code function

--- Exercícios/Exercícios resolvidos/Aula 08 - CPF.py
+++ Exercícios/Exercícios resolvidos/Aula 08 - CPF.py
@@ -5,6 +5,6 @@ print('Bem vindo! Este programa informa se um CPF é válido.')
 cpf = input('Digite um CPF (apenas números): ')
 
 if CPF.validate(cpf):
-    print(f'\nO CPF {CPF.format(cpf)} é VÁLIDO')
+    print(f'\nO CPF {CPF.format(cpf)} é válido')
 else:
     print(f'\nO CPF {CPF.format(cpf)} NÃO é válido')
--- Exercícios/Exercícios resolvidos/Exercício 017.py
+++ Exercícios/Exercícios resolvidos/Exercício 017.py
@@ -1,4 +1,15 @@
 # Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo retângulo.
 # Calcule e mostre o comprimento da hipotenusa.
 # H² = CA² + CO²
-# Usar PypI
+# Usar Python Packages
+
+from math import hypot
+
+print('Bem vindo! Este programa calcula a hipotenusa de um triângulo retângulo.')
+
+cateto_oposto = float(input('Comprimento do cateto oposto: '))
+cateto_adjacente = float(input('Comprimento do cateto adjacente: '))
+
+hipotenusa = hypot(cateto_oposto, cateto_adjacente)
+
+print(f'\nA hipotenusa mede {hipotenusa:.2f}')
