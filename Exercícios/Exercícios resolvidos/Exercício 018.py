70f01097c962d686593979e7cdc9f5817dd65b16
Mateus Adada <mateusadada@gmail.com>
2023-04-24 23:28:03 -0300

    Update Exercício 018.py
    Added the code and a note about code function

--- Exercícios/Exercícios resolvidos/Exercício 018.py
+++ Exercícios/Exercícios resolvidos/Exercício 018.py
@@ -1,2 +1,15 @@
 # Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo.
 # Usar Python Packages
+
+from math import radians, sin, cos, tan
+
+print('Bem vindo! Este programa mostra o valor do seno, cosseno e tangente de qualquer ângulo.')
+
+angulo_graus = float(input('Digite um ângulo: '))
+
+angulo_radiano = radians(angulo_graus)
+
+print(f'\nO ângulo de {angulo_graus}° tem as seguintes características:'
+      f'\n- Seno: {sin(angulo_radiano):.2f}'
+      f'\n- Cosseno: {cos(angulo_radiano):.2f}'
+      f'\n- Tangente: {tan(angulo_radiano):.2f}')
