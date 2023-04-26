05093192fce2097d52a7f75473a886d6f718de2b
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-04-26 14:05:07 -0300

    Update Aula 04 - Primeiro comando.py
    Added a note about code function and improved the semantics

--- Exercícios/Exercícios resolvidos/Aula 04 - Primeiro comando.py
+++ Exercícios/Exercícios resolvidos/Aula 04 - Primeiro comando.py
@@ -1,7 +1,11 @@
 # Crie um script que leia o nome, a idade e o peso de uma pessoa e mostre na tela.
 
+print('Bem vindo ao programa que exibe o nome, a idade e o peso de uma pessoa!')
+
 nome = input('Qual é o seu nome? ')
 idade = input('Qual é a sua idade? ')
 peso = input('Qual é o seu peso? ')
 
-print(nome, idade, peso)
+print(f'\nNome: {nome}'
+      f'\nIdade: {idade} anos'
+      f'\nPeso: {peso} kg')
