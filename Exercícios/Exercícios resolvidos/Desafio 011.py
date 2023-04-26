27daa1cd79729ece0769c6580954ba6b355864fd
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-04-26 16:25:00 -0300

    Update Desafio 011.py
    Added a note about code function

--- Exercícios/Exercícios resolvidos/Desafio 011.py
+++ Exercícios/Exercícios resolvidos/Desafio 011.py
@@ -1,7 +1,9 @@
 # Leia a largura e a altura de uma parede em metros, calcule a área e a quantidade de tinta necessária para pintá-la.
 # Sabendo que cada litro de tinta pinta uma área de 2 m².
 
-largura = float(input('Digite a largura de uma parede: '))
+print('Bem vindo ao programa de cálculo de tinta para pintar uma parede!')
+
+largura = float(input('Digite a largura: '))
 altura = float(input('Digite a altura: '))
 area = largura * altura
 tinta_necessaria = area / 2
