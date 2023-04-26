9310c9d78104e66ca3826b843a008a135e14b609
Mateus Adada <mateusadada@gmail.com>
2023-04-25 22:26:11 -0300

    Update Exercício 019.py
    Added the code and a note about code function

--- Exercícios/Exercícios resolvidos/Exercício 019.py
+++ Exercícios/Exercícios resolvidos/Exercício 019.py
@@ -1,3 +1,15 @@
 # Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele
 # lendo o nome dos alunos e escrevendo na tela o nome do escolhido.
 # import random
+
+from random import choice
+
+print('Bem vindo ao programa de sorteio de nome para apagar o quadro!')
+
+nome_1 = input('*** Informe os nomes ***'
+               '\n1º aluno: ')
+nome_2 = input('2º aluno: ')
+nome_3 = input('3º aluno: ')
+nome_4 = input('4º aluno: ')
+
+print(f'\nO aluno escolhido foi {choice([nome_1, nome_2, nome_3, nome_4])}')
