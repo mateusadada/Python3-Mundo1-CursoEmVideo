81593d31f1ff05d522b77e30aa8d1ebc9edc9d08
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-04-26 16:12:38 -0300

    Update Desafio 002.py
    Added a note about code function

--- Exercícios/Exercícios resolvidos/Desafio 002.py
+++ Exercícios/Exercícios resolvidos/Desafio 002.py
@@ -1,5 +1,7 @@
 # Crie um script que leia o dia, o mês e o ano de nascimento de uma pessoa e mostre uma mensagem com a data formada.
 
+print('Bem vindo ao programa que exibe o dia, o mês e o ano de nascimento de uma pessoa!')
+
 dia = input('Qual o dia do seu nascimento? ')
 mes = input('Qual o mês? ')
 ano = input('Qual o ano? ')
