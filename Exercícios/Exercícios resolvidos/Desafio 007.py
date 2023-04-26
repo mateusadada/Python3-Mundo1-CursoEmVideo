0c0c0ab7d0082bb617558ece1599e1aab5ace833
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-04-26 16:20:44 -0300

    Update Desafio 007.py
    Added a note about code function

--- Exercícios/Exercícios resolvidos/Desafio 007.py
+++ Exercícios/Exercícios resolvidos/Desafio 007.py
@@ -1,5 +1,7 @@
 # Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
 
+print('Bem vindo ao programa de cálculo da média de duas notas!')
+
 nota1 = float(input('Digite a primeira nota: '))
 nota2 = float(input('Digite a segunda nota: '))
 media = (nota1 + nota2)/2
