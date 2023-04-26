7ff5ec1727d167a6dcef2ae64549b6037aae6a51
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-04-26 16:18:50 -0300

    Update Desafio 006.py
    Added a note about code function

--- Exercícios/Exercícios resolvidos/Desafio 006.py
+++ Exercícios/Exercícios resolvidos/Desafio 006.py
@@ -1,5 +1,7 @@
 # Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
 
+print('Bem vindo ao programa de cálculo que exibe o dobro, o triplo e a raiz quadrada de um número!')
+
 numero = int(input('Digite um número: '))
 
 print(f'\nDobro: {numero * 2}')
