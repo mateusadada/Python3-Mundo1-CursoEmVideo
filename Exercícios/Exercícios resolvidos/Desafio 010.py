6e716945bbfb9aa384fa32e22dd08b398ba28e92
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-04-26 16:23:29 -0300

    Update Desafio 010.py
    Added a note about code function

--- Exercícios/Exercícios resolvidos/Desafio 010.py
+++ Exercícios/Exercícios resolvidos/Desafio 010.py
@@ -1,5 +1,7 @@
 # Leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar. 1 US$ = R$ 3.27
 
+print('Bem vindo ao programa de conversão de reais em dólares!')
+
 dinheiro = float(input('Quanto dinheiro você tem? R$ '))
 dolares = dinheiro / 3.27
 
