59717f7c78184c9f33e22db4a7d2d92bc4272a83
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-04-26 16:11:03 -0300

    Update Desafio 001.py
    Added a note about code function

--- Exercícios/Exercícios resolvidos/Desafio 001.py
+++ Exercícios/Exercícios resolvidos/Desafio 001.py
@@ -1,5 +1,7 @@
 # Crie um script que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas de acordo com o valor digitado.
 
+print('Bem vindo ao programa que faz uma saudação com o nome da pessoa!')
+
 nome = input('Qual é o seu nome? ')
 
 print(f'Olá, {nome}! Seja bem-vindo!')
