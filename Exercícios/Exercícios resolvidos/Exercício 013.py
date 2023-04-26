e255bfa9d21002928bd1e5e084538b0352b1dcb8
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-04-26 16:43:02 -0300

    Update Exercício 013.py
    Added a note about code function

--- Exercícios/Exercícios resolvidos/Exercício 013.py
+++ Exercícios/Exercícios resolvidos/Exercício 013.py
@@ -1,5 +1,7 @@
 # Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
 
+print('Bem vindo ao programa de cálculo de aumento de salário!')
+
 salario = float(input('Qual é o salário do funcionário? R$ '))
 novo_salario = salario * 1.15
 
