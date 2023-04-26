bd4e0464cdece5403be3d898c8c60240e619b1b1
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-04-26 16:17:35 -0300

    Update Desafio 005.py
    Added a note about code function

--- Exercícios/Exercícios resolvidos/Desafio 005.py
+++ Exercícios/Exercícios resolvidos/Desafio 005.py
@@ -1,5 +1,7 @@
 # Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor.
 
+print('Bem vindo ao programa que exibe o sucessor e o antecessor de um número!')
+
 numero = int(input('Digite um número: '))
 
 print(f'\nSucessor: {numero + 1}')
