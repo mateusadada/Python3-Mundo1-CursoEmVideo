82ab3701933a5e044f506ef1e4f6d90abef038a6
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-04-26 16:44:31 -0300

    Update Exercício 015.py
    Added a note about code function

--- Exercícios/Exercícios resolvidos/Exercício 015.py
+++ Exercícios/Exercícios resolvidos/Exercício 015.py
@@ -1,6 +1,8 @@
 # Escreva um programa que pergunte a quantidade de Km percorridos por um carro alugado e a quantidade de dias
 # pelos quais ele foi alugado. Calcule o preço a pagar, sabendo que o carro custa R$60 por dia e R$0,15 por Km rodado.
 
+print('Bem vindo ao programa de cálculo de carro alugado!')
+
 KM = int(input('Quantos KM rodados? '))
 dias = int(input('Quantos dias alugados? '))
 valor = dias * 60 + KM * 0.15
