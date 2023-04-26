b0a64768e8ed96ae3fb5bf2281b94a59050a12eb
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-04-26 16:30:59 -0300

    Update Exercício 002.py
    Added a note about code function

--- Exercícios/Exercícios resolvidos/Exercício 002.py
+++ Exercícios/Exercícios resolvidos/Exercício 002.py
@@ -1,5 +1,7 @@
 # Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
 
+print('Bem vindo ao programa que exibe uma saudação com o nome!')
+
 nome = input('Qual é o seu nome? ')
 
 print(f'Olá, {nome}. É um prazer te conhecer!')
