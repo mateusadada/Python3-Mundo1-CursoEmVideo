f3e77266d6d5de9a1b17d1607bc7a51db0a0257e
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-04-26 16:27:24 -0300

    Update Desafio 012.py
    Added a note about code function

--- Exercícios/Exercícios resolvidos/Desafio 012.py
+++ Exercícios/Exercícios resolvidos/Desafio 012.py
@@ -1,6 +1,8 @@
 # Leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
 
-preco_atual = float(input('Digite o preço de um produto: R$ '))
+print('Bem vindo ao programa de cálculo de desconto de um produto!')
+
+preco_atual = float(input('Digite o preço: R$ '))
 preco_novo = preco_atual * 0.95
 
 print(f'\nPreço com 5% de desconto: R$ {preco_novo:.2f} reais')
