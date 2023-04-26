55b84223f9db1ccded2bbebd909ffca852ddb599
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-04-26 16:08:24 -0300

    Update Aula 08 - B.py
    Added a note about code function and improved syntax

--- Exercícios/Exercícios resolvidos/Aula 08 - B.py
+++ Exercícios/Exercícios resolvidos/Aula 08 - B.py
@@ -1,7 +1,9 @@
 import random
 
+print('Bem vindo ao programa que sorteia um número entre 1 e 100 e entre 0 e 1!')
+
 numero = random.randint(1, 100)
 numero2 = random.random()
 
 print(f'Entre 1 e 100: {numero}'
-      f'\nAleatório entre 0 e 1: {numero2}')
+      f'\nEntre 0 e 1: {numero2}')
