f273d16f931d6700e2aa4a892ef5b6ad96b764ee
Mateus Adada <mateusadada@gmail.com>
2023-05-02 23:31:59 -0300

    Update Exercício 020.py
    Added the code and a note about code function

--- Exercícios resolvidos/Exercício 020.py
+++ Exercícios resolvidos/Exercício 020.py
@@ -1,3 +1,14 @@
 # O mesmo professor do desafio 019 quer sortear a ordem de apresentação de trabalhos dos alunos.
 # Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
-# import random = ver se precisa incluir o código para poder sortear
+
+from random import sample
+
+print('Bem vindo ao programa que sorteia a ordem de apresentação de trabalhos!')
+
+alunos = []
+
+for e in range(0, 4):
+    alunos.append(input(f'Nome do {e + 1}º aluno: '))
+
+print('\nA ordem de apresentação será:'
+      f'\n{sample(alunos, k=4)}')
