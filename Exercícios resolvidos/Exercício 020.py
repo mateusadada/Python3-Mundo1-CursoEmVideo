82fe3d1dd37f588745ae0312223bb96599aac8ee
Mateus Adada <mateusadada@gmail.com>
2023-05-02 23:38:34 -0300

    Update Exercício 020.py
    Replaced the function sample to shuffle

--- Exercícios resolvidos/Exercício 020.py
+++ Exercícios resolvidos/Exercício 020.py
@@ -1,7 +1,7 @@
 # O mesmo professor do desafio 019 quer sortear a ordem de apresentação de trabalhos dos alunos.
 # Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
 
-from random import sample
+from random import shuffle
 
 print('Bem vindo ao programa que sorteia a ordem de apresentação de trabalhos!')
 
@@ -10,5 +10,7 @@ alunos = []
 for e in range(0, 4):
     alunos.append(input(f'Nome do {e + 1}º aluno: '))
 
+shuffle(alunos)
+
 print('\nA ordem de apresentação será:'
-      f'\n{sample(alunos, k=4)}')
+      f'\n{alunos}')
