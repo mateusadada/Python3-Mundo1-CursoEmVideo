4713ad47efc24fd7d6d0eafa503fccbaf9922489
Mateus Adada <mateusadada@gmail.com>
2023-05-06 14:09:36 -0300

    Update Exercício 021.py
    Added the code, a note about code function and the sound

--- Exercícios resolvidos/Exercício 021.py
+++ Exercícios resolvidos/Exercício 021.py
@@ -1,1 +1,11 @@
 # Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
+
+import pygame
+
+print('\nBem vindo ao programa que reproduz uma música!')
+
+pygame.mixer.init()
+pygame.mixer.music.load('Exercício 021.mp3')
+pygame.mixer.music.play()
+
+input('\nTocando a música Journey - Dont Stop Believin')
