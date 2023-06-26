e7903d39c6cafe6d66cb4ad6afe4dc87a64f3b77
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-06-26 12:44:29 -0300

    Create Aula 09 - A to D
    Added the code and notes

---
+++ Exercícios resolvidos/Aula 09 - B.py
@@ -0,0 +1,37 @@
+print('Bem vindo ao programa de análise de string!\n')
+
+# 20 índices e 21 caracteres
+frase = 'Curso em Vídeo Python'
+
+# quantidade de índices/tamanho
+print(len(frase))
+
+# quantidade de 'o'
+print(frase.count('o'))
+
+# quantidade de 'o' do índice 0 até o 12
+print(frase.count('o', 0, 13))
+
+# mostra a posição/índice que começa 'deo'
+print(frase.find('deo'))
+
+# se a string não existe = -1
+print(frase.find('Android'))
+
+# se 'Curso' tem na variável frase
+print('Curso' in frase)
+
+# não substitui direto na variável
+print(frase.replace('Python', 'Android'))
+
+# deixa tudo em maiúsculo. Colocar os () porque é método
+print(frase.upper())
+
+# deixa tudo em minúsculo
+print(frase.lower())
+
+# tudo em minúsculo, menos o índice 0
+print(frase.capitalize())
+
+# cada início de palavra fica em maiúscula
+print(frase.title())
