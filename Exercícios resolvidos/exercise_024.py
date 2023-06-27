693d62bf8f3ec66f505c018d52f883e49b86763a
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-06-27 12:57:47 -0300

    Update exercise_024.py
    Added the code, a note about function and replaced syntax/semantics

--- Exercícios resolvidos/exercise_024.py
+++ Exercícios resolvidos/exercise_024.py
@@ -1,1 +1,7 @@
-# Crie um programa que leia o nome de uma cidade diga se ela começa ou não com o nome “SANTO”.
+# Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome “Santo”.
+
+print('Bem vindo ao programa que verifica se o nome de uma cidade começa com "Santo"!')
+
+cidade = str(input('Digite o nome de uma cidade: ')).strip()
+
+print(f'\nO nome começa com "Santo"? {(cidade[0:5]).upper() == "SANTO"}')
