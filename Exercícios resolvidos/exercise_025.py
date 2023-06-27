f35df2eb65d7f70b8bb69d1de9b63997331f7cdb
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-06-27 16:38:31 -0300

    Update exercise_025.py
    Added the code and a note about funtion

--- Exercícios resolvidos/exercise_025.py
+++ Exercícios resolvidos/exercise_025.py
@@ -1,1 +1,7 @@
 # Crie um programa que leia o nome de uma pessoa e diga se ela tem “SILVA” no nome.
+
+print('Bem vindo ao programa que verifica se tem "SILVA" no nome de uma pessoa!')
+
+nome = str(input('Nome completo: ')).strip()
+
+print(f'\nTem SILVA no nome? {"SILVA" in nome.upper()}')
