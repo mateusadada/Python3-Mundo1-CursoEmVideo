edac4d39bf4184c8af99592a12284a359db61f5c
Mateus Adada <mateusadada@gmail.com>
2023-06-26 22:56:33 -0300

    Update exercise_021.py
    Update file

--- Exercícios resolvidos/exercise_021.py
+++ Exercícios resolvidos/exercise_021.py
@@ -5,7 +5,7 @@ import pygame
 print('\nBem vindo ao programa que reproduz uma música!')
 
 pygame.init()
-pygame.mixer.music.load('Exercício 021.mp3')
+pygame.mixer.music.load('exercise_021.mp3')
 pygame.mixer.music.play()
 
 input('\nTocando a música Journey - Dont Stop Believin')
