9f88b82efec2ea953455b015a8acc73e22ae304f
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-06-27 16:44:26 -0300

    Update exercise_027.py
    Added the code and a note about function

--- Exercícios resolvidos/exercise_027.py
+++ Exercícios resolvidos/exercise_027.py
@@ -1,2 +1,10 @@
 # Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome
 # separadamente.
+
+print('Bem vindo ao programa que exibe o primeiro e o último nome separadamente!')
+
+nome = str(input('Nome completo: ')).strip()
+nome_convertido = nome.split()
+
+print(f'\nPrimeiro nome: {nome_convertido[0]}'
+      f'\nÚltimo nome: {nome_convertido[len(nome_convertido) - 1]}')
