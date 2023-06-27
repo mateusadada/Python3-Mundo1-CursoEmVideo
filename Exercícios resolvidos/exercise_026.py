a11ef3ece3bb4aba71b6c620b4aa48454cf6983b
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-06-27 16:41:24 -0300

    Update exercise_026.py
    Added the code, a note about function and replaced syntax/semantics

--- Exercícios resolvidos/exercise_026.py
+++ Exercícios resolvidos/exercise_026.py
@@ -1,2 +1,12 @@
-# Faça um programa que leia uma frase pelo teclado e mostre quantas vezes aparece a letra “A”, em que posição ela
-# aparece a primeira vez e em que posição ela aparece a última vez.
+# Faça um programa que leia uma frase pelo teclado e mostre:
+# - Quantas vezes aparece a letra “A”;
+# - A posição que ela aparece a primeira vez;
+# - A posição que ela aparece a última vez.
+
+print('Bem vindo ao programa que exibe várias informações a partir de uma frase!')
+
+frase = str(input('Digite uma frase: ')).strip()
+
+print(f'\nQuantas vezes aparece a letra "a/A": {frase.lower().count("a")}'
+      f'\nA posição que aparece por primeiro: {frase.lower().find("a") + 1}'
+      f'\nA posição que aparece por último: {frase.lower().rfind("a") + 1}')
