5e591403b0c2909cb0b7e60b1af89b59d0a90694
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-06-28 12:44:36 -0300

    Update exercise_029.py
    Added the code and a note about function

--- Exercícios resolvidos/exercise_029.py
+++ Exercícios resolvidos/exercise_029.py
@@ -1,2 +1,12 @@
 # Escreva um programa que leia a velocidade de um carro. Se ele ultrapassar 80Km/h, mostre uma mensagem dizendo que
 # ele foi multado. A multa vai custar R$7,00 por cada Km acima do limite.
+
+print('Bem vindo ao programa de multas!')
+
+velocidade = int(input('Digite a velocidade (km/h): '))
+
+if velocidade > 80:
+    print(f'\nMultado por ter excedido {velocidade - 80} km/h'
+          f'\nValor a pagar: R$ {(velocidade - 80) * 7:.2f} reais')
+else:
+    print('\nVelocidade dentro do permitido. Liberado!')
