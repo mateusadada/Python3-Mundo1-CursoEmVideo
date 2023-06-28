1a0c870820f52d901b54245e5912d48bcce5724e
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-06-28 16:32:15 -0300

    Update exercise_033.py
    Added the code and a note about function

--- Exercícios resolvidos/exercise_033.py
+++ Exercícios resolvidos/exercise_033.py
@@ -1,1 +1,21 @@
 # Faça um programa que leia três números e mostre qual é o maior e qual é o menor.
+
+print('Bem vindo ao programa de cálculo do maior e menor número!')
+
+numero = []
+
+for i in list(range(3)):
+    numero.append(int(input(f'{i + 1}º número: ')))
+
+maior = numero[0]
+menor = numero[0]
+
+for i in list(range(1, 3)):
+    if numero[i] > maior:
+        maior = numero[i]
+
+    if numero[i] < menor:
+        menor = numero[i]
+
+print(f'\nMaior: {maior}'
+      f'\nMenor: {menor}')
