b04ecbb8ea2f591595bd13aae203dcb25791a48f
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-06-28 13:06:49 -0300

    Update exercise_032.py
    Added the code and a note about function

--- Exercícios resolvidos/exercise_032.py
+++ Exercícios resolvidos/exercise_032.py
@@ -1,1 +1,16 @@
 # Faça um programa que leia um ano qualquer e mostre se ele é bissexto.
+
+print('Bem vindo ao programa de cálculo de ano bissexto!')
+
+ano = int(input('Digite um ano: '))
+
+if ano % 4 == 0:
+    if ano % 100 == 0:
+        if ano % 400 == 0:
+            print(f'\nO ano {ano} é bissexto!')
+        else:
+            print(f'\nO ano {ano} é NÃO bissexto!')
+    else:
+        print(f'\nO ano {ano} é bissexto!')
+else:
+    print(f'\nO ano {ano} é NÃO bissexto!')
