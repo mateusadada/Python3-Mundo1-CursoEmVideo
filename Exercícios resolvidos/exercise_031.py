1b0144d344ca8c434adf1c5e357d636975df1fd2
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-06-28 12:59:05 -0300

    Update exercise_031.py
    Added the code and a note about function

--- Exercícios resolvidos/exercise_031.py
+++ Exercícios resolvidos/exercise_031.py
@@ -1,2 +1,11 @@
 # Desenvolva um programa que pergunte a distância de uma viagem em Km. Calcule o preço da passagem, cobrando R$ 0,50
 # por km para viagens de até 200 km e R$ 0,45 parta viagens mais longas.
+
+print('Bem vindo ao programa de cálculo de passagem!')
+
+distancia = int(input('Informe a distância (km): '))
+
+if distancia <= 200:
+    print(f'\nPassagem (R$ 0,50 por km): R$ {distancia * 0.5:.2f} reais')
+else:
+    print(f'\nPassagem (R$ 0,45 por km): R$ {distancia * 0.45:.2f} reais')
