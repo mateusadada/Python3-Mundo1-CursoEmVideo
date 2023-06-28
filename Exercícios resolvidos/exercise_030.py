03428d26edebda347a20bd55482db00f31735af1
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-06-28 12:56:37 -0300

    Update exercise_030.py
    Added the code and a note about functon

--- Exercícios resolvidos/exercise_030.py
+++ Exercícios resolvidos/exercise_030.py
@@ -1,1 +1,10 @@
 # Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR.
+
+print('Bem vindo ao programa de cálculo de par ou ímpar!')
+
+numero = int(input('Digite um número inteiro: '))
+
+if numero % 2 == 0:
+    print(f'\nO número {numero} é PAR')
+else:
+    print(f'\nO número {numero} é ÍMPAR')
