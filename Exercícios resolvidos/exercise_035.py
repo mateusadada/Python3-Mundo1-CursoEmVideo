35721b66a4b9e7276e536c390f322ad29fa7a6b4
Mateus Adada <mateusadada@gmail.com>
2023-06-28 22:15:00 -0300

    Update exercise_035.py
    Added the code and a note about function

--- Exercícios resolvidos/exercise_035.py
+++ Exercícios resolvidos/exercise_035.py
@@ -1,2 +1,13 @@
 # Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se elas podem ou não formar um
 # triângulo.
+
+print('Bem vindo ao programa de verificação de triângulo!')
+
+reta1 = float(input('1º reta: '))
+reta2 = float(input('2º reta: '))
+reta3 = float(input('3º reta: '))
+
+if reta1 + reta2 < reta3 or reta1 + reta3 < reta2 or reta2 + reta3 < reta1:
+    print('\nÉ possível formar um triângulo!')
+else:
+    print('\nNÃO é possível formar um triângulo!')
