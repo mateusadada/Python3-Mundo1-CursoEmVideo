27728eda95440758220975eb743d8b28e290117a
Mateus Adada <mateusadada@gmail.com>
2023-06-28 22:39:33 -0300

    Update exercise_035.py
    Replaced 'or' to 'and'

--- Exercícios resolvidos/exercise_035.py
+++ Exercícios resolvidos/exercise_035.py
@@ -7,7 +7,7 @@ reta1 = float(input('1º reta: '))
 reta2 = float(input('2º reta: '))
 reta3 = float(input('3º reta: '))
 
-if reta1 + reta2 > reta3 or reta1 + reta3 > reta2 or reta2 + reta3 > reta1:
+if reta1 + reta2 > reta3 and reta1 + reta3 > reta2 and reta2 + reta3 > reta1:
     print('\nÉ possível formar um triângulo!')
 else:
     print('\nNÃO é possível formar um triângulo!')
