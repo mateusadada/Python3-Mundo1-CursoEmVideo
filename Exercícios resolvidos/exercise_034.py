d9322d3f38051fa22e241a2f73beec913d6ea002
Mateus Adada <mateusadada@gmail.com>
2023-06-28 22:07:22 -0300

    Update exercise_034.py
    Added the code and a note about function

--- Exercícios resolvidos/exercise_034.py
+++ Exercícios resolvidos/exercise_034.py
@@ -1,2 +1,13 @@
 # Escreva um programa que pergunte o salário de um funcionário e calcule o valor do seu aumento. Para salários
 # superiores a R$1250,00, calcule um aumento de 10%. Para os inferiores ou iguais, o aumento é de 15%.
+
+print('Bem vindo ao programa de cálculo de aumento de salário!')
+
+salario = float(input('Informe o salário: R$ '))
+
+if salario > 1250:
+    print(f'\nAumento de 10%!'
+          f'\nNovo salário: R$ {salario * 1.1:.2f} reais')
+else:
+    print(f'\nAumento de 15%!'
+          f'\nNovo salário: R$ {salario * 1.15:.2f} reais')
